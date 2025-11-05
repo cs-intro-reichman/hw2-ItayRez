17f85933705c6d49f066d65170ee1391b2aaf9ea
Itay Reznik <itay.reznik@post.runi.ac.il>
2025-11-05 16:58:30 +0200

    Done

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,30 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+
+          String wordUpper = args[0].toUpperCase();
+          int amount = Integer.parseInt(args[1]);
+          int length = wordUpper.length();
+          String anLetters = "AEFHILMNORSX";
+
+
+
+          for (int i = 0; i < length ; i++){
+
+                 if (anLetters.indexOf(wordUpper.charAt(i)) != -1){
+                     System.out.println("Give me an " + wordUpper.charAt(i) + ": " + wordUpper.charAt(i) + "!");
+                } else {
+                     System.out.println("Give me a  " + wordUpper.charAt(i) + ": " + wordUpper.charAt(i) + "!");
+                }
+
+        }
+        
+          System.out.println("What does that spell?");
+
+          for (int j = 0; j < amount ; j++){
+
+                System.out.println(wordUpper + "!!!");
+
+          }
+
         }
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,37 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int n = Integer.parseInt(args[0]);
+		String mode = args[1];
+		int i = 1;
+		
+		for (int count = 1 ; count <= n ; count++ ){ 
+			i = count;
+			int stepCount = 1; 
+
+			if (mode.equals("v")){ 
+				System.out.print(i + " ");
+			}
+			while(true) { 
+			    if (i%2 != 0){ 
+				    i = i*3 + 1;
+					if (mode.equals("v")){
+				        System.out.print(i + " ");
+				    }
+			    } else { 
+				    i /=2;
+					if (mode.equals("v")){ 
+				        System.out.print(i + " ");
+					}
+			    } 
+				stepCount++;
+				 if (i==1) { 
+					break;
+				}
+		    } if(mode.equals("v")){
+				System.out.println("(" + stepCount + ")"); 
+			}
+		} 
+		System.out.println("Every one of the first " + n + " hailstone sequences reached " + i + ".");
 	}
 }
